045df8c0faf6f2de39ef57aa3076f841bddbdef7
mrattez <mrattez@student.42nice.fr>
2023-05-12 14:04:18 +0200

    ➖ Removed some includes

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,21 +1,3 @@
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <vector>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-
-#include "config/Config.hpp"
-#include "config/ServerBlock.hpp"
-
-#include "utils/Logger.hpp"
-#include "http/Client.hpp"
-#include "http/ClientManager.hpp"
-#include "http/Socket.hpp"
 #include "http/Server.hpp"
 
 int	main(void)
