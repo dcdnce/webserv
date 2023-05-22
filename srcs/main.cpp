02cbf8116ef6a1d745b438e70aacbeea8e368eaf
dcdnce <pforesti@student.42nice.fr>
2023-05-22 16:08:44 +0200

    🔀 Merged main.cpp

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,4 +1,6 @@
 #include "config/Config.hpp"
+#include "http/Socket.hpp"
+#include "http/ClientManager.hpp"
 #include "http/Multiplexer.hpp"
 
 int	main(int ac, char **av)
@@ -15,6 +17,9 @@ int	main(int ac, char **av)
 		http::Multiplexer multiplexer(config);
 
 		multiplexer.listen();
+		// http::Request	r;
+		// r.parse("GET /api/users/123 HTTP/1.1\nHost: example.com\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36\nAccept: application/json\r\n\r\nprout");
+		// std::cout << r << std::endl;
 	}
 	catch (const std::exception &e)
 	{
