929aa2579f13e41ec7eadf0bf5911c4714298f1c
dcdnce <pforesti@student.42nice.fr>
2023-06-07 16:38:39 +0200

    ✨ Cookie Login added

---
+++ cgi/create_cookie.py
@@ -0,0 +1,26 @@
+import cgi
+import http.cookies
+import os
+
+params = cgi.FieldStorage()
+
+username = params.getvalue('username')
+cookie = [
+	f"Set-Cookie:weebservUsername = {username}",
+	"Max-Age = 3600",
+	#"Domain = localhost", # No domain so it works on all machines (if no domain is provided, the value will be the host of the url of current document)
+	"Path = /",
+]
+
+print("; ".join(cookie))
+print ("Content-type:text/html\r\n")
+print ("<html>")
+print ("<head>")
+print("<meta charset=\"UTF-8\">")
+print("<script type=\"text/javascript\">")
+print("window.location.href = \"/\"")
+print("</script>")
+print ("</head>")
+print ("</html>")
+
+
---
+++ www/js/login.js
@@ -0,0 +1,24 @@
+function getCookieValueByName(cookieName)
+{
+	decodedCookies = decodeURIComponent(document.cookie).split(';');
+	cookieName += "=";
+	for (let i = 0 ; i < decodedCookies.length ; i++)
+	{
+		startIndex = (decodedCookies[i][0] == ' ');
+		if (decodedCookies[i].indexOf(cookieName) == startIndex)
+			return (decodedCookies[i].substring(cookieName.length));
+	}
+	return (null);
+}
+
+window.onload = function()
+{
+	cookieValue = getCookieValueByName("weebservUsername");
+	if (cookieValue != null) {
+		const p = document.querySelector('p.display-none');
+		p.classList.toggle("display-none")
+		p.innerHTML += " " + cookieValue + ' !';
+
+		document.getElementById("cookie_form").classList.toggle("display-none");
+	}
+}
